5a0b7f23425876ca74ebfeeb32a6af5c814d83e4
Andrea <ageria20@gmail.com>
2024-07-09 20:27:08 +0200

    trying to add somo animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -65,14 +65,14 @@ const Home = () => {
         <Container className="bg-light text-secondary p-4 rounded mb-5 details shadow">
           <Row className="px-2 mt-5  mb-2 justify-content-between text-center">
             <Col xs={12} md={5}>
-              <Container className="d-flex flex-column align-items-center text-center border rounded-2 py-3">
+              <Container className="d-flex flex-column align-items-center text-center border rounded-2 py-3 info">
                 <h5>TODAY</h5>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="30"
                   height="30"
                   fill="currentColor"
-                  className="mb-3 bi bi-brightness-high-fill "
+                  className="mb-3 bi bi-brightness-high-fill todayIcon"
                   viewBox="0 0 16 16"
                 >
                   <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
@@ -81,7 +81,7 @@ const Home = () => {
             </Col>
 
             <Col xs={12} md={5}>
-              <Container className="d-flex flex-column align-items-center justidy-content-center text-center border rounded-2 py-4">
+              <Container className="d-flex flex-column align-items-center justidy-content-center text-center border rounded-2 py-4 info">
                 <h5 className="mb-3">
                   Low {degreesConversion(weather.main.temp_min)} °C / High {degreesConversion(weather.main.temp_max)}° C
                 </h5>
@@ -91,7 +91,7 @@ const Home = () => {
           </Row>
           <Row className="px-0 mt-3 justify-content-between">
             <Col xs={12} md={5}>
-              <Container className=" d-flex flex-column text-center border rounded-2 py-3">
+              <Container className=" d-flex flex-column text-center border rounded-2 py-3 info">
                 <h5>HUMIDITY</h5>
                 <Container>
                   <svg
@@ -99,7 +99,7 @@ const Home = () => {
                     width="16"
                     height="16"
                     fill="currentColor"
-                    className="bi bi-droplet-fill"
+                    className="bi bi-droplet-fill "
                     viewBox="0 0 16 16"
                   >
                     <path d="M8 16a6 6 0 0 0 6-6c0-1.655-1.122-2.904-2.432-4.362C10.254 4.176 8.75 2.503 8 0c0 0-6 5.686-6 10a6 6 0 0 0 6 6M6.646 4.646l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448c.82-1.641 1.717-2.753 2.093-3.13" />
@@ -110,7 +110,7 @@ const Home = () => {
             </Col>
 
             <Col xs={12} md={5}>
-              <Container className="text-center border rounded-2 py-3">
+              <Container className="text-center border rounded-2 py-3 info">
                 <h5>WIND</h5>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
