6729dd3c69ef2e65c38c6b0d6efd497903a53dbb
Andrea <ageria20@gmail.com>
2024-07-10 12:15:11 +0200

    temp svg added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -85,7 +85,17 @@ const Home = () => {
                 <h5 className="mb-3">
                   Low {degreesConversion(weather.main.temp_min)} °C / High {degreesConversion(weather.main.temp_max)}° C
                 </h5>
-                <p className="mb-0">{degreesConversion(weather.main.feels_like).toUpperCase()}</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-thermometer-half"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V6.5a.5.5 0 0 1 1 0v4.585a1.5 1.5 0 0 1 1 1.415" />
+                  <path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1" />
+                </svg>
               </Container>
             </Col>
           </Row>
