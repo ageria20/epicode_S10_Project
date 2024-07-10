8150823b40dfce5377e27c68a77d346dd1815e45
Andrea <ageria20@gmail.com>
2024-07-10 12:01:45 +0200

    animazioni svg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -99,7 +99,7 @@ const Home = () => {
                     width="16"
                     height="16"
                     fill="currentColor"
-                    className="bi bi-droplet-fill "
+                    className="bi bi-droplet-fill humidityIcon"
                     viewBox="0 0 16 16"
                   >
                     <path d="M8 16a6 6 0 0 0 6-6c0-1.655-1.122-2.904-2.432-4.362C10.254 4.176 8.75 2.503 8 0c0 0-6 5.686-6 10a6 6 0 0 0 6 6M6.646 4.646l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448c.82-1.641 1.717-2.753 2.093-3.13" />
@@ -117,7 +117,7 @@ const Home = () => {
                   width="16"
                   height="16"
                   fill="currentColor"
-                  className="bi bi-wind"
+                  className="bi bi-wind windIcon"
                   viewBox="0 0 16 16"
                 >
                   <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5" />
